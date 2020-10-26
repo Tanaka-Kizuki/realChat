d14212993aeb8c1d8b9e1025589c64876885196c
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-27 00:44:43 +0900

    view create html

---
+++ Desktop/realChat/resources/views/chat.blade.php
@@ -0,0 +1,9 @@
+<html>
+     <body>
+          <div id="chat">
+               <textarea v-model="message"></textarea>
+               <br>
+               <button type="button">送信</button>
+          </div>
+     </body>
+</html>
