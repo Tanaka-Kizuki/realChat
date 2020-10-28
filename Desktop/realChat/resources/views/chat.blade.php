ed0e2ee9531c793c9dbc1b33da6de3cb748d9f3c
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-28 19:44:44 +0900

    view create script

--- Desktop/realChat/resources/views/chat.blade.php
+++ Desktop/realChat/resources/views/chat.blade.php
@@ -3,7 +3,31 @@
           <div id="chat">
                <textarea v-model="message"></textarea>
                <br>
-               <button type="button">送信</button>
+               <button type="button" @click="send()">送信</button>
           </div>
+
+          <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+
+          <script>
+          
+          new Vue({
+               el:'#chat',
+               data:{
+                    message:''
+               },
+               methods:{
+                    send:function() {
+                         const url = '/ajax/chat';
+                         const params = {message: this.message};
+                         axios.post(url,params).then((response) => {
+                              this.message= "";
+                         })
+                    }
+               }
+          })
+
+          </script>
+
      </body>
 </html>
